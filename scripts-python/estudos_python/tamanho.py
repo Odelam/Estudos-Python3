d93cd9a2ccb92d8f91bd823c810156dcad21fa78
Odelam Sá <odelamsa@gmail.com>
2020-10-02 00:28:29 -0300

    adicionando script

--- scripts-python/estudos_python/tamanho.py
+++ scripts-python/estudos_python/tamanho.py
@@ -12,7 +12,11 @@ def tamanho_aprox(tamanho, kylobyte_eh_1024_bytes=True):
     if tamanho < 0:
         raise ValueError('Número não pode ser negativo!')
 
-    multiplo = 1024 if kylobyte_eh_1024_bytes else 1000
+    # multiplo = 1024 if kylobyte_eh_1024_bytes else 1000
+    if kylobyte_eh_1024_bytes:  # True
+        multiplo = 1024
+    else:                       # False
+        multiplo = 1000
     for sufix in sufixos[multiplo]:
         tamanho /= multiplo
         if tamanho < multiplo:
@@ -22,5 +26,5 @@ def tamanho_aprox(tamanho, kylobyte_eh_1024_bytes=True):
 
 
 if __name__ == '__main__':
-    print(tamanho_aprox(1000000000000, False))
-    print(tamanho_aprox(1000000000000))
+    print(tamanho_aprox(298756788, False))
+    print(tamanho_aprox(298756788))
