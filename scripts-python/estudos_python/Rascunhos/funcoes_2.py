7122e3001cb7b1c71ab062d8dedcb6b334cf9c8c
Odelam Sá <odelamsa@gmail.com>
2020-10-03 18:14:45 -0300

    adicionando script

--- scripts-python/estudos_python/Rascunhos/funcoes_2.py
+++ scripts-python/estudos_python/Rascunhos/funcoes_2.py
@@ -1,15 +1,61 @@
-def raiz_quadrada(n):
-    return n ** 2
+import random
 
 
-print(raiz_quadrada(3))
+# def raiz_quadrada(n):
+#     return n ** 2
+#
+#
+# print(raiz_quadrada(3))
+#
+#
+# def raizquadrada(n):
+#     root = n / 2
+#     for c in range(20):
+#         root = (1 / 2) * (root + (n / root))
+#     return root
+#
+#
+# print(f"{raizquadrada(12):.2f}")
+
+# def mostrar_resposta(num_resposta):
+#     if num_resposta == 1:
+#         return 'É certo!'
+#     elif num_resposta == 2:
+#         return 'Você está quase lá!'
+#     elif num_resposta == 3:
+#         return 'Não desista!'
+#     elif num_resposta == 4:
+#         return 'Não pense em parar agora!'
+#
+#
+# num_resposta = random.randint(1, 4)
+# mensagem = mostrar_resposta(num_resposta)
+#
+# print(mensagem)
+# # print(mostrar_resposta(random.randint(1, 4)))
+#
+
+def a():
+    print(f"a() começa.")
+    b()
+    d()
+    print('a() retorna')
 
 
-def raizquadrada(n):
-    root = n / 2
-    for c in range(20):
-        root = (1 / 2) * (root + (n / root))
-    return root
+def b():
+    print("b() começa")
+    c()
+    print("b() retorna")
 
 
-print(f"{raizquadrada(12):.2f}")
+def c():
+    print("c() retorna")
+    print('c() retorna')
+
+
+def d():
+    print("d() começa")
+    print('d() retorna')
+
+
+print(a())
