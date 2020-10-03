b0e07b31ca58bbd3e9b6b48f0808eeb0c1778de4
Odelam Sá <odelamsa@gmail.com>
2020-10-03 16:16:34 -0300

    adicionando script

--- scripts-python/estudos_python/forca_jogo.py
+++ scripts-python/estudos_python/forca_jogo.py
@@ -3,6 +3,24 @@ def jogar_forca():
     print("Jogo da forca!")
     print("*" * 20)
 
+    palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+        chute = str(input("Qual letra? ")).strip().upper()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra.upper():
+                letras_acertadas[index] = letra
+            index += 1
+        print(letras_acertadas)
+        print("Jogando")
+
     print("Fim do jogo!")
 
 
