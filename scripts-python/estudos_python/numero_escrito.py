98841e9a98cf62e853721abf1f5b173edaf3f969
Odelam Sá <odelamsa@gmail.com>
2020-10-15 02:15:28 -0300

    adicionando script

--- scripts-python/estudos_python/numero_escrito.py
+++ scripts-python/estudos_python/numero_escrito.py
@@ -1,13 +1,21 @@
 numeros_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
+resposta = ' '
 
-while True:
-    numero = int(input("Digite um número entre 0 e 10: "))
-    if numero >= 0 and numero <= 10:
+while resposta != 'N':
+    while True:
+        numero = int(input("Digite um número entre 0 e 10: "))
+        if 0 <= numero <= 10:
+            break
+        print("Número fora do range.", end=' ')
+
+    for p, n in enumerate(numeros_extenso):
+        if p == numero:
+            print(f'Você digitou o número: {n.title()}.')
+
+    resposta = str(input("Deseja continuar? [S/N] ")).strip().upper()
+    if resposta == 'N':
+        print()
+        print("Até mais!")
         break
-    print("Número fora do range.", end=' ')
-
-for p, n in enumerate(numeros_extenso):
-    if p == numero:
-        print(f'Você digitou o número: {n.title()}.')
 
 
