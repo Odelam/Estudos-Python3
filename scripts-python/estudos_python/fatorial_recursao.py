b6b53b28a6e49d8107c16a7b296f751f7bbb722c
Odelam <odelamsa@gmail.com>
2020-10-23 06:36:17 -0300

    adicionando script

--- scripts-python/estudos_python/fatorial_recursao.py
+++ scripts-python/estudos_python/fatorial_recursao.py
@@ -6,4 +6,57 @@ def fatorial(n):
         return resultado
 
 
-print(fatorial(7))
+# l = [1, 2, (3, 4)]
+# print(l)
+# print(l[0])
+# print(l[2])
+# print(l[2][0])
+# print(l[-1][0])
+#
+# x, y = 2, 3
+# print('x', x)
+# print('y', y)
+# print()
+#
+# y, x = x, y
+# print('x', x)
+# print('y', y)
+#
+# class Ponto:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+# p = Ponto(1, 2)
+# q = Ponto(3, 4)
+#
+# print(f"p: {p.x,y}")
+# print(f"q: {q.x,y}")
+
+# class Ponto:
+#     x = 0
+#     y = 0
+#
+# p = Ponto()
+# Ponto.x = 1
+# p.x = 2
+# p.y = 3
+# Ponto.y = 4
+# print(f"Ponto.x:{Ponto.x}")
+# print(f"p.x:{p.x}")
+# print()
+# print(f"Ponto.y:{Ponto.y}")
+# print(f"p.y:{p.y}")
+
+class Cachorro:
+    def __init__(self, idade):
+        self.idade = idade
+
+
+class Dalmata(Cachorro):
+    def __init__(self, idade, pedigree):
+        super(Dalmata, self).__init__(idade)
+        self.pedigree = pedigree
+
+d = Dalmata(3, True)
+print(d.idade, d.pedigree)
